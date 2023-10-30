37a7b8b30815ca29eeca4dac64f173d6b5b826de
ardymiqbal <145640557+ardymiqbal@users.noreply.github.com>
2023-10-30 11:08:45 +0800

    Create AutocompleteAndTemplates.java

---
+++ AutocompleteAndTemplates.java
@@ -0,0 +1,11 @@
+public class AutocompleteAndTemplates {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("hallo!");
+        int Kalkulator = 10+2;
+        System.out.println(Kalkulator);
+    }
+}
